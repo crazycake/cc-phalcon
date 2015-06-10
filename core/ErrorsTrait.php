ddc4872bdee43c92b56bbb2064230cf4bae2fe69
Nicolas Pulido <npulidom@gmail.com>
2015-06-10 14:05:33 -0300

    new error traits

---
+++ core/ErrorsTrait.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Errors Trait
+ * This class has the common public actions for error pages
+ * Requires a Frontend or Backend Module with CoreController
+ * @author Nicolas Pulido <nicolas.pulido@crazycake.cl>
+ */
+
+namespace CrazyCake\Core;
+
+trait ErrorsTrait
+{
+	//....
+
+    /* --------------------------------------------------- § -------------------------------------------------------- */
+
+	/**
+     * View - 404 Not found page
+     */
+    public function notFoundAction()
+    {
+        if($this->request->isAjax())
+            $this->_sendJsonResponse(404);
+    }
+
+    /**
+     * View - 500 Error page
+     */
+    public function internalAction()
+    {
+        if($this->request->isAjax())
+            $this->_sendJsonResponse(500);
+    }
+
+    /**
+     * View - Old Browser
+     */
+    public function oldBrowserAction()
+    {
+        //...
+    }
+
+    /**
+     * View - Expired page (pages that needs temporal token to access)
+     */
+    public function expiredAction()
+    {
+        //...
+    }
+}
