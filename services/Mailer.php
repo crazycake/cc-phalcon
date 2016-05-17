9936a4e4a01e5f665db9631691d4934b759ea1cf
Nicolas Pulido <npulidom@gmail.com>
2016-05-17 12:22:28 -0300

    eliminado tags

--- services/Mailer.php
+++ services/Mailer.php
@@ -66,7 +66,6 @@ trait Mailer
 
         //send JSON response
         $this->jsonResponse(200);
-        return;
     }
 
     /**
@@ -96,15 +95,12 @@ trait Mailer
         $this->mailer_conf["data_email"] = $user->email;
         $this->mailer_conf["data_url"]   = $this->baseUrl($uri);
 
-        //get HTML
-        $html_raw = $this->inlineHtml("activation", $this->mailer_conf);
         //set message properties
         $subject = $this->mailer_conf["trans"]["SUBJECT_ACTIVATION"];
         $to      = $this->mailer_conf["data_email"];
-        $tags    = ["account", "activation"];
 
         //sends async email
-        return $this->sendMessage($html_raw, $subject, $to, $tags);
+        return $this->sendMessage("activation", $subject, $to);
     }
 
     /**
@@ -137,14 +133,11 @@ trait Mailer
         $this->mailer_conf["data_url"]   = $this->baseUrl($uri);
         $this->mailer_conf["data_token_expiration"] = $tokens_class::$TOKEN_EXPIRES_THRESHOLD;
 
-        //get HTML
-        $html_raw = $this->inlineHtml("passwordRecovery", $this->mailer_conf);
         //set message properties
         $subject = $this->mailer_conf["trans"]["SUBJECT_PASSWORD"];
         $to      = $this->mailer_conf["data_email"];
-        $tags    = array("account", "password", "recovery");
         //sends async email
-        return $this->sendMessage($html_raw, $subject, $to, $tags);
+        return $this->sendMessage("passwordRecovery", $subject, $to);
     }
 
 	/**
@@ -161,8 +154,13 @@ trait Mailer
         if(!is_file(self::$MAILING_CSS_FILE))
             throw new Exception("Mailer cant find mailing CSS file: ".self::$MAILING_CSS_FILE);
 
+        $template_view = "mailing/$template";
+
+        if(!$this->simpleView->exists($template_view))
+            throw new Exception("Cant find mailing view $template_view");
+
         //get the style file
-        $html = $this->simpleView->render("mailing/$template", $this->mailer_conf);
+        $html = $this->simpleView->render($template_view, $this->mailer_conf);
         $css  = file_get_contents(self::$MAILING_CSS_FILE);
 
         //HTML inliner
@@ -203,7 +201,7 @@ trait Mailer
      * @param array $message_data - Must contains keys "name", "email" & "message"
      * @return json response
      */
-    public function sendAdminMessage($message_data)
+    public function sendAdminMessage($message_data = [])
     {
     	if (empty($message_data))
     		return false;
@@ -211,42 +209,37 @@ trait Mailer
         //set message properties
         $subject = isset($message_data["subject"]) ? $message_data["subject"] : $this->config->app->name;
         $to      = isset($message_data["to"]) ? $message_data["to"] : $this->config->app->emails->contact;
-        $tags    = ["contact", "support"];
 
         //add prefix "data" to each element in array
-        $view_data = array_combine( array_map(function($k) { return "data_".$k; }, array_keys($message_data)), $message_data);
-
-        //get HTML
-        $html_raw = $this->inlineHtml("contact", $view_data);
+        $this->mailer_conf = array_combine( array_map(function($k) { return "data_".$k; }, array_keys($message_data)), $message_data);
 
         //sends async email
-        return $this->sendMessage($html_raw, $subject, $to, $tags);
+        return $this->sendMessage("contact", $subject, $to);
     }
 
     /**
      *  Sends a message through mandrill API
-     * @param string $html_raw - The HTML raw string
+     * @param string $template - The template name
      * @param string $subject - The mail subject
      * @param mixed(string|array) $recipients - The receiver emails
-     * @param array $tags - Monitor tags
      * @param array $attachments - Array with sub-array(s) with content, type and name props
      * @param boolean $async - Async flag, defaults to true
      * @return string
      */
-    public function sendMessage($html_raw, $subject, $recipients, $tags = [], $attachments = [], $async = true)
+    public function sendMessage($template, $subject, $recipients, $attachments = [], $async = true)
     {
         //validation
-        if (empty($html_raw) || empty($subject) || empty($recipients))
+        if (empty($template) || empty($subject) || empty($recipients))
             throw new Exception("Mailer::sendMessage -> Invalid params data for sending email");
 
         //parse recipients
         if (is_string($recipients))
-            $recipients = array($recipients);
+            $recipients = [$recipients];
 
         $to = [];
         //create mandrill recipient data struct, push emails to array
         foreach ($recipients as $email)
-            array_push($to, array("email" => $email)); //optional name (display name) & type (defaults "to").
+            array_push($to, ["email" => $email]); //optional name (display name) & type (defaults "to").
 
         //set default subject
         if (empty($subject))
@@ -254,13 +247,12 @@ trait Mailer
 
         //Send message email!
         $message = [
-            "html"       => $html_raw,
+            "html"       => $this->inlineHtml($template),
             "subject"    => $subject,
             "from_email" => $this->config->app->emails->sender,
             "from_name"  => $this->config->app->name,
             "to"         => $to,
-            "tags"       => $tags
-            //"inline_css" => true //same as _inlineHtml method. (generates more delay time)
+            "tags"       => []
         ];
 
         //append attachments
