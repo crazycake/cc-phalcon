974bbb69b0a54013a6d9f554900df8dfa5d8cd50
Nicolas Pulido <npulidom@gmail.com>
2016-07-26 02:36:40 -0300

    hotfixes

--- webpacks/src/modules/forms.js
+++ webpacks/src/modules/forms.js
@@ -315,24 +315,23 @@ export default new function() {
      * @param  {string} string - An input string
      * @return {string}
      */
-    self.stripHtml = function(string = "") {
+    self.stripHtml = function(string = null) {
+
+        if(_.isNull(string))
+            return "";
 
         return string.replace(/<\/?[^>]+(>|$)/g, "");
     };
 
 
     /**
-     * Check if given URL is valid.
+     * Check if given URL starts with http
      * @method isUrl
      * @return {Boolean}
      */
     self.isUrl = function(url = "") {
 
-        var regex = new RegExp(
-            /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i
-        );
-
-        return url.match(regex);
+        return url.substring(0, 4) === "http";
     };
 
     /**
