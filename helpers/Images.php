163710ce831547e299a101a45501a91fb86b4552
Nicolas Pulido <npulidom@gmail.com>
2016-08-16 23:26:20 -0300

    fixed get resized image path

--- helpers/Images.php
+++ helpers/Images.php
@@ -27,7 +27,13 @@ class Images
      */
 	public static function resizedImagePath($path = "", $key = "TH")
 	{
-		return preg_replace("/\\.([0-9a-z]+)(?:[\\?#]|$)/i", "_".$key.".$1?", $path);
+		$new_url = preg_replace("/\\.([0-9a-z]+)(?:[\\?#]|$)/i", "_".$key.".$1?", $path);
+
+        //remove single question marks
+        if(substr($new_url, -1) === "?")
+            $new_url = substr($new_url, 0, strlen($new_url) - 1);
+
+        return $new_url;
 	}
 
     /**
