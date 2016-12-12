02f6a2bdc0576d5da31d79c2bb227f880e5be4e6
Nicolas Pulido <npulidom@gmail.com>
2016-12-12 18:20:36 -0300

    cdn to local

--- phalcon/AppServices.php
+++ phalcon/AppServices.php
@@ -116,7 +116,7 @@ class AppServices
             $static_url = !empty($this->config->staticUrl) ? $this->config->staticUrl : false;
 
             //set static uri for assets, cdn only for production
-            if (!$static_url || APP_ENV !== "production")
+            if (!$static_url || APP_ENV === "local")
                 $static_url = APP_BASE_URL;
 
             $url->setStaticBaseUri($static_url);
