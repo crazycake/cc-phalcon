52fb39077e4893c3f49b69856897ba0b4ffda413
Nicolas Pulido <npulidom@gmail.com>
2017-11-03 12:02:19 -0300

    removed contact

--- controllers/Mailer.php
+++ controllers/Mailer.php
@@ -77,7 +77,7 @@ trait Mailer
 		], "POST");
 
 		$data["subject"] = "Contacto ".$this->config->name;
-		$data["to"]      = $this->config->emails->support ?? $this->config->emails->contact;
+		$data["to"]      = $this->config->emails->support;
 		//contents
 		$this->mailer_conf = array_merge($this->mailer_conf, $data);
 
