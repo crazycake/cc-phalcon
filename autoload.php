1ef71cb7367e7d79fe4a500d552896fdcd37a899
Nicolas Pulido <npulidom@gmail.com>
2018-06-09 12:19:58 -0400

    code order

--- autoload.php
+++ autoload.php
@@ -3,7 +3,7 @@
  * Autoload phar file
  */
 
-DEFINE('CORE_VERSION', '5ec0d36');
+DEFINE('CORE_VERSION', '6254731');
 
 // load App
 require "phalcon/App.php";
--- phalcon/AppServices.php
+++ phalcon/AppServices.php
@@ -262,13 +262,12 @@ class AppServices
 
 			//set domain for cookie params
 			$host = explode('.', parse_url(APP_BASE_URL, PHP_URL_HOST));
-			//ss($host[1].".".$host[0]);
-			// set session name (domain & cookie)
-			$session->setName($conf->namespace);
 
 			//session exceptions for shared cookies domain
 			session_set_cookie_params($expiration, "/", getenv("SESSION_DOMAIN") ?? implode(".", $host));
 
+			// set session name & start
+			$session->setName($conf->namespace);
 			$session->start();
 
 			return $session;
