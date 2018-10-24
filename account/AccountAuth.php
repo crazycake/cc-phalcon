e931a329ffd01d84fd1c69193e90324d0ba34ad2
Nicolas Pulido <npulidom@gmail.com>
2018-10-24 19:55:31 -0300

    actualizado account auth

--- account/AccountAuth.php
+++ account/AccountAuth.php
@@ -216,14 +216,11 @@ trait AccountAuth
 			// check user pending flag
 			$user = $entity::getById($user_id);
 
-			if (!$user || $user->flag != "pending")
+			if (!$user || $user->flag == "disabled")
 				throw new Exception("invalid user or missing 'pending' flag, userID: $user->id");
 
 			// save new account flag state
 			$entity::updateProperty($user_id, "flag", "enabled");
-
-			// remove activation token
-			//$this->deleteToken($user_id, "activation");
 
 			// custom behavior event
 			if (method_exists($this, "onActivationSuccess")) {
