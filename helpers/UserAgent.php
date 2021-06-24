e64104c5776a2244e0729847efcd186f266089fe
Nicolas Pulido <npulidom@gmail.com>
2021-06-24 13:17:00 -0400

    agregado webpage test

--- helpers/UserAgent.php
+++ helpers/UserAgent.php
@@ -50,7 +50,7 @@ class UserAgent
 		$data["is_legacy"]     = $this->_isUserAgentLegacy($data);
 
 		// special cases
-		$data["is_analyser"] = !empty(preg_match('/prerendercrawler|chrome-lighthouse|gtmetrix/i', $this->user_agent));
+		$data["is_analyser"] = !empty(preg_match('/prerendercrawler|chrome-lighthouse|gtmetrix|ptst/i', $this->user_agent));
 
 		return $data;
 	}
